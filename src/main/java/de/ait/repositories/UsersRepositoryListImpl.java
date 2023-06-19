3304ba0f6d933244856727dec9df83b6376eaa3b
Yuliia Tsaruk <tsarukyula87@gmail.com>
2023-06-19 15:25:01 +0200

    add users at List FamilyMembers im UsersRepositoryListImpl

--- src/main/java/de/ait/repositories/UsersRepositoryListImpl.java
+++ src/main/java/de/ait/repositories/UsersRepositoryListImpl.java
@@ -6,23 +6,25 @@ import de.ait.models.User;
 import java.util.ArrayList;
 import java.util.List;
 
-//public class UsersRepositoryListImpl implements UsersRepository {
-    /*
-
+public class UsersRepositoryListImpl implements UsersRepository {
 
     @Override
     public List<FamilyMember> findAll() {
-        FamilyMember user = new User();
-        FamilyMember user1 = new User();
-        FamilyMember user2 = new User();
+        FamilyMember user1 =
+                new FamilyMember("Ivan", "Ivanov", 35, "father");
+        FamilyMember user2 =
+                new FamilyMember("Mari", "Ivanova", 30, "mother");
+        FamilyMember user3 =
+                new FamilyMember("Daniil", "Ivanov", 7, "son");
+        FamilyMember user4 =
+                new FamilyMember("Liana", "Ivanova", 5, "daughter");
 
         List<FamilyMember> users = new ArrayList<>();
-        users.add(user);
         users.add(user1);
-        users.add(FamilyMember
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
 
         return users;
-
+    }
 }
-         */
-//}
