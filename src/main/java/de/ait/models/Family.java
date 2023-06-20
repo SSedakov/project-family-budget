ab8592ba498e9c0788f66000ee7c255e233c0c12
Nataliia <pnata1978@gmail.com>
2023-06-20 22:03:15 +0200

    Add class IncomeExpenses in class Family and create List<IncomeExpenses> incomeExpenses

--- src/main/java/de/ait/models/Family.java
+++ src/main/java/de/ait/models/Family.java
@@ -1,5 +1,6 @@
 package de.ait.models;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,9 +17,53 @@ public class Family {
         this.id = UUID.randomUUID().toString();
     }
 
-    public Family() {
-    }
+    private static class IncomeExpenses {
+    private String title;
+    private int sum;
+    private String id;
 
+        @Override
+        public String toString() {
+            return "IncomeExpenses{" +
+                    "title='" + title + '\'' +
+                    ", sum=" + sum +
+                    ", id='" + id + '\'' +
+                    '}';
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public int getSum() {
+            return sum;
+        }
+
+        public void setSum(int sum) {
+            this.sum = sum;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public IncomeExpenses(String title, int sum, String id) {
+            this.title = title;
+            this.sum = 0;
+            this.id = UUID.randomUUID().toString();
+
+        }
+    }
+    List<IncomeExpenses> incomeExpenses =new ArrayList<IncomeExpenses>(); // Создали пустой лист доходов-расходов
+                                                                          // на основе созданного выше вложенного класса
     public void addBudget(int cash){
         balance = balance + ((cash/100) * 90);
         asideMoney = asideMoney + ((cash/100) * 10);
