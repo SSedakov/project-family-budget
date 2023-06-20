779f43321459fb3ad0cb27ffa8b76a74a49f7ae5
Yuliia Tsaruk <tsarukyula87@gmail.com>
2023-06-20 19:08:35 +0200

    add class Accumulation in models and metod writeTransactionsToFile in services

--- src/main/java/de/ait/services/UsersService.java
+++ src/main/java/de/ait/services/UsersService.java
@@ -1,11 +1,13 @@
 package de.ait.services;
 
-import de.ait.models.Family;
-import de.ait.models.FamilyMember;
+import de.ait.models.*;
 
 import java.util.List;
 
 public interface UsersService {
     List<String> getNames();
     int addBudget(List<FamilyMember> family);
+
+    public void writeTransactionsToFile
+            (List<Income> incomes, List<Expense> expenses, List<Accumulation> accumulations);
 }
--- src/main/java/de/ait/services/UsersServiceImpl.java
+++ src/main/java/de/ait/services/UsersServiceImpl.java
@@ -1,9 +1,11 @@
 package de.ait.services;
 
-import de.ait.models.Family;
-import de.ait.models.FamilyMember;
+import de.ait.models.*;
 import de.ait.repositories.UsersRepository;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class UsersServiceImpl implements UsersService {
@@ -33,5 +35,38 @@ public class UsersServiceImpl implements UsersService {
         return 0;
     }
 
+    @Override
+    public void writeTransactionsToFile
+            (List<Income> incomes, List<Expense> expenses, List<Accumulation> accumulations) {
+        try (BufferedWriter writer =
+                     new BufferedWriter(new FileWriter("transactions.txt"))){
+            writer.write("Поступления денежных средств:\n");
+            for (Income income : incomes){
+                writer.write("Назначение: " + income.getPurpose()
+                        + ", Сумма: " + income.getSum()
+                        + ", Дата: " + income.getDate()
+                        + ", ID: " + income.getId() + "\n");
+            }
+            writer.write("Списания денежных средств:\n");
+            for (Expense expense : expenses){
+                writer.write("Назначение: " + expense.getPurpose()
+                        + ", Сумма: " + expense.getSum()
+                        + ", Дата: " + expense.getDate()
+                        + ", ID: " + expense.getId() + "\n");
+            }
+            writer.write("Накопление денежных средств:\n");
+            for (Accumulation accumulation : accumulations){
+                writer.write("Назначение: " + accumulation.getPurpose()
+                        + ", Сумма: " + accumulation.getSum()
+                        + ", Дата: " + accumulation.getDate()
+                        + ", ID: " + accumulation.getId() + "\n");
+            }
+            writer.close();
+        } catch (IOException e){
+            System.out.println("Ошибка при записи транзакций в файл: " + e.getMessage());
+        }
+    }
+
 
 }
+
